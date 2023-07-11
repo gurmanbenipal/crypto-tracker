bc0f0ba84cddea557cebb4967ca12788c56afee6
gurmanbenipal <gurmxnbenipal@gmail.com>
2023-07-11 16:50:46 -0700

    oauth done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Crypto Market Tracker' });
+  res.render('index');
 });
+
+
+
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+    // Which passport strategy is being used?
+    'google',
+    {
+      // Requesting the user's profile and email
+      scope: ['profile', 'email'],
+      // Optionally force pick account every time
+      // prompt: "select_account"
+    }
+  ));
+  
+  // Google OAuth callback route
+  router.get('/oauth2callback', passport.authenticate(
+    'google',
+    {
+      //send them to the search page after they log in
+      successRedirect: '/search',
+      //send them back to the landing page if they dont log in
+      failureRedirect: '/'
+    }
+  ));
+  
+  // OAuth logout route
+  router.get('/logout', function(req, res){
+    req.logout(function() {
+      //will send them to my landing page when they log out
+        res.redirect('/');
+    });
+  });
+
+
+
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var methodOverride = require('method-override');
+var passport = require('passport');
+
+
+require('dotenv').config();
+
+require('./config/database');
+
+require('./config/passport');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var searchRouter = require('./routes/search');
+
+
 
 var app = express();
 
@@ -18,9 +32,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(methodOverride('_method'));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req,res,next){
+  res.locals.user = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
